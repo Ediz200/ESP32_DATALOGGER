7682eb7b3b012467d9de0e7c2ffc658e1f1bb4cb
Ediz200 <ediz.tikiz@hotmail.com>
2024-08-18 17:07:09 +0200

    first commit

---
+++ .pio/build/esp32dev/libb99/SD/sd_diskio_crc.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/libb99/SD/sd_diskio_crc.c.o: \
+ C:/Users/edizt/.platformio/packages/framework-arduinoespressif32/libraries/SD/src/sd_diskio_crc.c
---
+++ src/main.cpp
@@ -0,0 +1,203 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+// Libraries for SD card
+#include "FS.h"
+#include "SD.h"
+#include <SPI.h>
+
+//DS18B20 libraries
+#include "DHT.h"
+
+// Libraries to get time from NTP Server
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+// Define deep sleep options
+uint64_t uS_TO_S_FACTOR = 1000000;  // Conversion factor for micro seconds to seconds
+// Sleep for 10 minutes = 600 seconds
+uint64_t TIME_TO_SLEEP = 15;
+
+// Replace with your network credentials
+const char* ssid     = "Hoppala4";
+const char* password = "Tekirdag2011!";
+
+// Define CS pin for the SD card module
+#define SD_CS 5
+
+// Save reading number on RTC memory
+char readingID[] = "DHT11";
+
+String dataMessage;
+
+// Data wire is connected to ESP32 GPIO 7
+#define DHTPIN 32
+#define DHTTYPE DHT11
+// Setup a oneWire instance to communicate with a OneWire device
+DHT dht(DHTPIN, DHTTYPE);
+
+// Temperature Sensor variables
+float temperature;
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// Variables to save date and time
+String formattedDate;
+String dayStamp;
+String timeStamp;
+
+// Append data to the SD card (DON'T MODIFY THIS FUNCTION)
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+// Function to get temperature
+void getReadings(){
+  temperature = dht.readTemperature(); // Temperature in Celsius
+    
+  if (isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+}
+
+// Function to get date and time from NTPClient
+void getTimeStamp() {
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  // The formattedDate comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+  formattedDate = timeClient.getFormattedDate();
+  Serial.println(formattedDate);
+
+  // Extract date
+  int splitT = formattedDate.indexOf("T");
+  dayStamp = formattedDate.substring(0, splitT);
+  Serial.println(dayStamp);
+  // Extract time
+  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+  Serial.println(timeStamp);
+}
+
+// Write the sensor readings on the SD card
+void logSDCard() {
+  dataMessage = String(readingID) + "," + String(dayStamp) + "," + String(timeStamp) + "," + 
+                String(temperature) + "\r\n";
+  Serial.print("Save data: ");
+  Serial.println(dataMessage);
+  appendFile(SD, "/data.txt", dataMessage.c_str());
+}
+
+// Write to the SD card (DON'T MODIFY THIS FUNCTION)
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+void setup() {
+  // Start serial communication for debugging purposes
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+
+  // Initialize a NTPClient to get time
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(7200);
+
+  // Initialize SD card
+  SD.begin(SD_CS);  
+  if(!SD.begin(SD_CS)) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+  if(cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("ERROR - SD card initialization failed!");
+    return;    // init failed
+  }
+
+  // If the data.txt file doesn't exist
+  // Create a file on the SD card and write the data labels
+  File file = SD.open("/data.txt");
+  if(!file) {
+    Serial.println("File doens't exist");
+    Serial.println("Creating file...");
+    writeFile(SD, "/data.txt", "Reading ID, Date, Hour, Temperature \r\n");
+  }
+  else {
+    Serial.println("File already exists");  
+  }
+  file.close();
+
+  // Enable Timer wake_up
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+  // Start the DallasTemperature library
+  dht.begin(); 
+
+  getReadings();
+  getTimeStamp();
+  logSDCard();
+  
+  // Start deep sleep
+  Serial.println("DONE! Going to sleep now.");
+  esp_deep_sleep_start(); 
+}
+
+void loop() {
+  // The ESP32 will be in deep sleep
+  // it never reaches the loop()
+}
+
